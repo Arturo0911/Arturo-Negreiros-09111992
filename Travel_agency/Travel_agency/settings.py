57da282fa0833960d9fc60da30b28a13800d784e
arturo <anegreiross@outlook.com>
2021-05-03 23:16:21 -0500

    Connection with Mongodb betweem Django successfully

--- Travel_agency/Travel_agency/settings.py
+++ Travel_agency/Travel_agency/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Currency_formats',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'Travel_agency.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'country-currency',
     }
 }
 
