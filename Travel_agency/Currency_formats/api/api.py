81079415f4c292cf11f19064e852c7516b7d47aa
arturo <anegreiross@outlook.com>
2021-05-05 01:31:32 -0500

    added swagger interface

--- Travel_agency/Currency_formats/api/api.py
+++
@@ -1,3 +0,0 @@
-from rest_framework.views import APIView
-from Currency_formats.models import Country
-from Currency_formats.api.serializers import CountrySerializer
--- Travel_agency/Travel_agency/settings.py
+++ Travel_agency/Travel_agency/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Currency_formats',
     'rest_framework',
-    'rest_framework_swagger',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
